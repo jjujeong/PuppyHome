435fa3d71d7b25b93dae323ae7e7194c0dbc55e8
comjeong <>
2024-11-12 10:58:56 +0900

    feat: add spring init

---
+++ src/main/java/org/example/puppyhome/PuppyHomeApplication.java
@@ -0,0 +1,13 @@
+package org.example.puppyhome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuppyHomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuppyHomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/puppyhome/PuppyHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.puppyhome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuppyHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
