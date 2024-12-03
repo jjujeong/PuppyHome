c45cefe61069cb6c1856bad338be88126d6ca84e
jinju-seo <jinju.seo1004@gmail.com>
2024-12-03 19:27:04 +0900

    modified: swagger 설정 초기 구현

--- src/main/java/org/example/puppyhome/PuppyHomeApplication.java
+++ src/main/java/org/example/puppyhome/PuppyHomeApplication.java
@@ -2,8 +2,11 @@ package org.example.puppyhome;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@EnableMongoRepositories
+//@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"org.example.puppyhome"})
 public class PuppyHomeApplication {
 
 	public static void main(String[] args) {
