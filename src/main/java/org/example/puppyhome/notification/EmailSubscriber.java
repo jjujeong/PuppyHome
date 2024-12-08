46917932057306d3fda626dd5c4e056ca01f35cd
comjeong <>
2024-12-08 18:27:39 +0900

    fixed : 문자열 잘못 출력되는 거 수정

--- src/main/java/org/example/puppyhome/notification/EmailSubscriber.java
+++ src/main/java/org/example/puppyhome/notification/EmailSubscriber.java
@@ -7,14 +7,14 @@ import java.util.List;
 public class EmailSubscriber implements Subscriber {
     private String email;
 
-    public EmailSubscriber(String appId) {
+    public EmailSubscriber(String email) {
         this.email = email;
     }
 
     @Override
     public String notify(List<Animal> animals) {
         StringBuilder notificationMessage = new StringBuilder();
-        notificationMessage.append("Sending app to: ").append(email).append("\n");
+        notificationMessage.append("Sending email to: ").append(email).append("\n");
 
         if (animals.isEmpty()) {
             notificationMessage.append("No animals matching your criteria.\n");
--- src/main/java/org/example/puppyhome/notification/PhoneSubscriber.java
+++ src/main/java/org/example/puppyhome/notification/PhoneSubscriber.java
@@ -7,14 +7,12 @@ import java.util.List;
 public class PhoneSubscriber implements Subscriber {
     private String phoneNum;
 
-    public PhoneSubscriber(String phoneNum) {
-        this.phoneNum = phoneNum;
-    }
+    public PhoneSubscriber(String phoneNum) {this.phoneNum = phoneNum;}
 
     @Override
     public String notify(List<Animal> animals) {
         StringBuilder notificationMessage = new StringBuilder();
-        notificationMessage.append("Sending app to: ").append(phoneNum).append("\n");
+        notificationMessage.append("Sending phone to: ").append(phoneNum).append("\n");
 
         if (animals.isEmpty()) {
             notificationMessage.append("No animals matching your criteria.\n");
