0ee56a2e8f91e4778653cda62877fdd4e8344392
comjeong <>
2024-12-08 11:28:59 +0900

    fixed : DbStorageApplication.java을 PuppyHomeApplication.java 에 병합

--- src/main/java/org/example/puppyhome/PuppyHomeApplication.java
+++ src/main/java/org/example/puppyhome/PuppyHomeApplication.java
@@ -1,22 +1,18 @@
 package org.example.puppyhome;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.example.puppyhome.crawling.CrawlingController;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @EnableMongoRepositories
 @SpringBootApplication(scanBasePackages = {"org.example.puppyhome"})
 public class PuppyHomeApplication implements CommandLineRunner {
 
-	@Autowired
-	private CrawlingController crawlingController;
-
 	public static void main(String[] args) {
-		SpringApplication.run(PuppyHomeApplication.class, args);
+		SpringApplication app = new SpringApplication(PuppyHomeApplication.class);
+		app.setAdditionalProfiles("dbstorage");
+		app.run(args);
 	}
 
 	@Override
